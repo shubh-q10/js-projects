4af1293266f96418f4e1be248b9bb3cdaf937497
Shubham <shubhamsingh705010@gmail.com>
2026-06-09 18:25:19 +0530

    unlimited color done

---
+++ unlimited_colors/script.js
@@ -0,0 +1,28 @@
+// generate a random color
+
+const randomcolor = function () {
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+let intervalId;
+
+const startchangingcolor = function () {
+  if (!intervalId) {
+    intervalId = setInterval(changeBgColor, 1000);
+  }
+  function changeBgColor() {
+    document.body.style.backgroundColor = randomcolor();
+  }
+};
+
+const stopchangingcolor = function () {
+  clearInterval(intervalId);
+  intervalId = null;
+};
+
+document.querySelector("#start").addEventListener("click", startchangingcolor);
+document.querySelector("#stop").addEventListener("click", stopchangingcolor);
